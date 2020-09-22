7d6a26f74cd9838f7f3d6d96546f53de5e06a9d3
Sebastian Degerman <sebbed89@hotmail.com>
2020-09-22 22:57:15 +0200

    Refactor code (arrow functions, removed stuff etc)

--- public/js/main.js
+++ public/js/main.js
@@ -7,26 +7,18 @@ const searchInput = $('#search-input');
 const volume = $('#volume');
 const socket = io.connect();
 
-// Getting information about the connected clients
-// by the server.
 socket.on('connectedCount', (count) => {
   $('#connected-count').html(count);
 });
 
-// Everytime a user pause the video it will pause the video
-// on the client side.
 socket.on('pauseVideo', () => {
   player.pauseVideo();
 });
 
-// Everytime a user syncs the video it will sync the video
-// at current time the client side.
 socket.on('syncVideo', () => {
   player.seekTo(player.getCurrentTime())
 });
 
-// Everytime a user search for a new video it will update the video
-// for both users.
 socket.on('searchVideo', (videoId) => {
   player.cueVideoById(videoId);
   setTimeout(() => {
@@ -35,25 +27,19 @@ socket.on('searchVideo', (videoId) => {
   }, 2000);
 });
 
-// Everytime a user plays the video it will play the video
-// on the client side.
 socket.on('playVideo', () => {
   player.playVideo();
 });
 
-// Timeline click
 socket.on('timelineClick', (timelineClick) => {
   player.seekTo(timelineClick);
 });
 
-// Check if the dialog close button is pressed and emit the username
-// to the server.
 $('.modal-close').click(() => {
   const usernameInput = $('#username-input').val();
   socket.emit('userJoin', usernameInput);
 });
 
-// If a user joins it will append it to the user list.
 socket.on('userJoin', (username) => {
   $('#user-list').append(`
         <li class="user">
@@ -65,7 +51,6 @@ socket.on('userJoin', (username) => {
     `);
 });
 
-// If a user leaves it will be removed from the user list.
 socket.on('userLeave', (username) => {
   const users = document.getElementById('user-list').getElementsByClassName('username');
   for (let i = 0; i < users.length; i++) {
@@ -77,7 +62,7 @@ socket.on('userLeave', (username) => {
   }
 });
 
-// This code loads the IFrame Player API code asynchronously.
+// This code loads the iframe Player API code async.
 let tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 let firstScriptTag = document.getElementsByTagName('script')[0];
@@ -103,7 +88,7 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
-// This method gets called when the player is ready to go.
+// This function gets called when the player is ready to go.
 function onPlayerReady(event) {
   startTimelineLoop();
   changeVideoInformation();
@@ -111,7 +96,7 @@ function onPlayerReady(event) {
 }
 
 // This method gets called whenever the state of the player changes.
-// If someone pressed the play button (state=1) this method will be called
+// If someone pressed the play button (state=1) this function will be called
 // and it will notify all other connected clients that the video plays.
 function onPlayerStateChange(event) {
   switch (event.data) {
@@ -137,8 +122,6 @@ function startTimelineLoop() {
   });
 }
 
-// Whenever a user clicks on the timeline to jump to a position,
-// the dot and the movie will jump to this position.
 timeline.click(function(event) {
   const offset = $(this).offset();
   const x = event.pageX - offset.left;
@@ -149,20 +132,14 @@ timeline.click(function(event) {
   });
 });
 
-// Whenever a user clicks on the play button,
-// it will tell the other clients to play/resume the video.
 playButton.click((event) => {
   onPlayClicked();
 });
 
-// Whenever a user clicks on the sync button,
-// it will tell the other clients to sync the video.
 syncButton.click((event) => {
   onSyncClicked();
 });
 
-// Whenever a user clicks on the pause button,
-// it will tell the other clients to puase the video.
 pauseButton.click((event) => {
   onPauseClicked();
 });
@@ -172,50 +149,43 @@ volume.on('input', (event) => {
   player.setVolume(volume);
 });
 
-function changeVideoInformation() {
+const changeVideoInformation = () => {
   $('#video-title').text(player.getVideoData().title);
   $('#video-id').text(player.getVideoData().video_id);
 }
 
-// This function is a helper function to perform everything,
-// whenever the play button is clicked.
-function onPlayClicked() {
+const onPlayClicked = () => {
   socket.emit('playerEvent', {
     'event': 'play'
   });
 }
 
-// This function is a helper function to perform everything,
-// whenever the sync button is clicked.
-function onSyncClicked() {
+const onSyncClicked = () => {
   socket.emit('playerEvent', {
     'event': 'sync'
   });
 }
 
-// This function is a helper function to perform everything,
-// whenever the pause button is clicked.
-function onPauseClicked() {
+const onPauseClicked = () => {
   socket.emit('playerEvent', {
     'event': 'pause'
   });
 }
 
-function onSearchEntered() {
+const onSearchEntered = () => {
   socket.emit('playerEvent', {
     'event': 'search',
     'videoId': searchInput.val()
   });
 }
 
-// Misc
 $(document).ready(function() {
   $('.modal').modal();
-
+  
   $('#username-dialog').modal({
     dismissible: false,
   });
-
+  
   $('#username-dialog').modal('open');
 
   $('#username-input').on('input', function() {
